2a8502be0a24d4d1554d34824a496741a9b5d5be
David <Davik50@gmail.com>
2025-04-16 19:24:04 +0200

    KS-L1-5 Time spent in space upd v3

--- src/main/kotlin/Lesson1/lesson1.5.kt
+++ src/main/kotlin/Lesson1/lesson1.5.kt
@@ -1,5 +1,9 @@
 package org.example.Lesson1
 
+fun println(format: String, s: String) {
+
+}
+
 fun main() {
     val totalSeconds: Int = 6480
     val secondsInMinute: Int = 60
@@ -9,7 +13,6 @@ fun main() {
     val remainingSeconds = totalSeconds % secondsInHour
     val minutes = remainingSeconds / secondsInMinute
     val seconds = remainingSeconds % secondsInMinute
-    val formattedTime = String.format("%02d:%02d:%02d".format(hours, minutes, seconds))
 
-    println("Time spent in space $formattedTime")
+    println("Time spent in space %02d:%02d:%02d".format(hours, minutes, seconds))
 }
