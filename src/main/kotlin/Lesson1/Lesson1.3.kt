c9a225956cb99ff420ec83e38d1ea8c9dfa78318
David <Davik50@gmail.com>
2025-04-16 15:33:01 +0200

    KS-L1-3 space flight

---
+++ src/main/kotlin/Lesson1/Lesson1.3.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson1
+
+fun main () {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("Year $year")
+//    println("hour $hour")
+//    println("minute $minute")
+
+    hour = "10"
+    minute = "55"
+    println("Landing time: $hour:$minute")
+}
