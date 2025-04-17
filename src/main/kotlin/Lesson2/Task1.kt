2db13b7bff346ddb7a990f838654b2b90e759d61
David <Davik50@gmail.com>
2025-04-17 13:41:51 +0200

    KS-L2-1 Task 1

---
+++ src/main/kotlin/Lesson2/Task1.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson2
+
+fun main() {
+    val student1 = 3f
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+    val avgGrade = (student1 + student2 + student3 + student4) / 4
+
+    println("Average grade - $avgGrade")
+}
