54c6ea9065459c0049513c998bfb89436cfe9a73
David <Davik50@gmail.com>
2025-04-18 12:44:45 +0200

    KS-3-1 Task 1

---
+++ src/main/kotlin/Lesson3/Task1.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson3
+
+fun main() {
+    var greeting = ""
+    val userName = "My Lord"
+
+    greeting = "Good afternoon, $userName! Glad to see you again."
+    println(greeting)
+
+    greeting = "Good evening, $userName! Glad to see you again."
+    println(greeting)
+}
