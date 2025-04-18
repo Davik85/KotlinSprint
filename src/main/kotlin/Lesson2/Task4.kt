45febc1277ad8e63d987baef04fb39b67514f21f
David <Davik50@gmail.com>
2025-04-18 09:49:55 +0200

    KS-2-4 Task 4

--- src/main/kotlin/Lesson2/Task4.kt
+++ src/main/kotlin/Lesson2/Task4.kt
@@ -1,12 +1,12 @@
 package org.example.Lesson2
 
-const val BUFF = 0.2
-
 fun main() {
+    val buff = 20f
+    val decimalBuff = buff / 100
     val crystalOre = 7
     val ironOre = 11
-    val crystalWithBuff = (crystalOre * BUFF).toInt()
-    val ironWithBuff = (ironOre * BUFF).toInt()
+    val crystalWithBuff = (crystalOre * decimalBuff).toInt()
+    val ironWithBuff = (ironOre * decimalBuff).toInt()
 
     println("Additionally extracted crystal ore - $crystalWithBuff")
     println("Additionally extracted iron ore - $ironWithBuff")
