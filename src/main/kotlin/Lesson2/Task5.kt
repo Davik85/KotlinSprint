4e8c3ed5f811b84d64e3e028e8bad857187ff254
David <Davik50@gmail.com>
2025-04-18 09:35:38 +0200

    KS-2-5 Task 5

--- src/main/kotlin/Lesson2/Task5.kt
+++ src/main/kotlin/Lesson2/Task5.kt
@@ -4,7 +4,8 @@ import kotlin.math.pow
 
 fun main() {
     val initialDeposit = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
+    val rateDecimal = interestRate / 100
     val years = 20
     //Формула расчёта сложных процентов по вкладам: X = Y × (1 + P/D)D, где:
     //X — итоговая сумма вклада вместе с полученными процентами;
@@ -12,7 +13,7 @@ fun main() {
     //P — процентная ставка в десятичном виде (делённая на 100);
     //D — количество периодов начисления процентов.
     // Возводим в степень при помощи .pow
-    val finalAmount = initialDeposit * (1.0 + interestRate).pow(years)
+    val finalAmount = initialDeposit * (1.0 + rateDecimal).pow(years)
 
     println("The amount of the deposit after $years years: ${"%,.3f".format(finalAmount)} rub")
 }
