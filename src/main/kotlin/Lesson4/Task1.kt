400dc751ffc3a4410dd6e4f6d5fe0f733fb61602
David <Davik50@gmail.com>
2025-04-23 09:28:46 +0200

    KS-4-1 Task 1 v2

--- src/main/kotlin/Lesson4/Task1.kt
+++ src/main/kotlin/Lesson4/Task1.kt
@@ -1,12 +1,13 @@
 package org.example.Lesson4
 
 fun main() {
-    val totalTables = 13
     val bookedToday = 13
     val bookedTomorrow = 9
 
-    val availableToday = bookedToday < totalTables
-    val availableTomorrow = bookedTomorrow < totalTables
+    val availableToday = bookedToday < TOTAL_TABLES
+    val availableTomorrow = bookedTomorrow < TOTAL_TABLES
 
     println("Available tables for today: $availableToday\nAvailable tables for tomorrow: $availableTomorrow")
 }
+
+const val TOTAL_TABLES = 13
