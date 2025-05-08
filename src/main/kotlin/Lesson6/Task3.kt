a923ef4af379839e4d73345a0bd2ba42ca43503a
David <Davik50@gmail.com>
2025-05-08 10:21:05 +0200

    KS-6-3 task2 timer upd v2

--- src/main/kotlin/Lesson6/Task3.kt
+++ src/main/kotlin/Lesson6/Task3.kt
@@ -2,20 +2,18 @@ package org.example.Lesson6
 
 fun main() {
     print("Enter the number of seconds to wait: ")
-    val seconds = readln().toIntOrNull()
+    var seconds = readln().toIntOrNull()
 
     if (seconds == null || seconds <= 0) {
         println("Invalid input. Please enter a positive number.")
         return
     }
 
-    var remaining = seconds
-
     println("Timer started for $seconds seconds...")
-    while (remaining > 0) {
-        println("Seconds left: ${remaining--}...")
+    while (seconds > 0) {
+        println("Seconds left: ${seconds--}...")
         Thread.sleep(1000)
     }
 
-    println("Time's up! $seconds seconds have passed.")
+    println("Time's up!")
 }
