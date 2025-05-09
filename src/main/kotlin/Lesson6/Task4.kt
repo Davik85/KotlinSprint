f65bb3dc52c0cf8bb6ec69d3be252cca167f7655
David <Davik50@gmail.com>
2025-05-09 03:45:06 +0200

    KS-6-4 task4 guessing game v3

--- src/main/kotlin/Lesson6/Task4.kt
+++ src/main/kotlin/Lesson6/Task4.kt
@@ -1,17 +1,15 @@
 package org.example.Lesson6
 
 fun main() {
+    var maxAttempts = 5
     val secretNumber = (1..9).random()
-    val maxAttempts = 5
-    var attempt = 1
-    var guess: Int?
 
     println("Welcome to the number guessing game!")
     println("Guess the number between 1 and 9. You have $maxAttempts attempts.")
 
     do {
         print("Enter your guess: ")
-        guess = readln().toIntOrNull()
+        val guess = readln().toIntOrNull()
 
         if (guess == null || guess !in 1..9) {
             println("Invalid input. Please enter a number between 1 and 9.")
@@ -22,13 +20,12 @@ fun main() {
             println("That was a magnificent game!")
             return
         } else {
-            if (attempt < maxAttempts) {
-                println("Wrong guess. Attempts left: ${maxAttempts - attempt}")
+            --maxAttempts
+            if (maxAttempts > 0) {
+                println("Wrong guess. Attempts left: $maxAttempts")
             }
-            attempt++
         }
-
-    } while (attempt <= maxAttempts && guess != secretNumber)
+    } while (maxAttempts > 0)
 
     println("Out of attempts! The number was $secretNumber.")
 }
