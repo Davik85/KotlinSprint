abe08ad00384db38de51cc6f261c1e8bcb7ba8d3
David <Davik50@gmail.com>
2025-05-13 18:22:08 +0200

    KS-8-1 Task1 Views

---
+++ src/main/kotlin/Lesson8/Task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lessom8
+
+fun main() {
+    val dailyViews = arrayOf(117, 221, 110, 174, 250, 270, 211)
+
+    val totalViews = dailyViews.sum()
+
+    println("Total ad views for the week: $totalViews")
+}
