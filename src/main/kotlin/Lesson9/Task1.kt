771dcff1e1f46c106751e532d6762370a89690bc
David <Davik50@gmail.com>
2025-05-15 15:33:46 +0200

    KS-9-1 Task1 ingredients

--- src/main/kotlin/Lesson9/Task1.kt
+++ src/main/kotlin/Lesson9/Task1.kt
@@ -3,10 +3,7 @@ package org.example.Lesson9
 fun main() {
     val ingredients = listOf("flour", "sugar", "eggs", "milk", "butter")
 
-    println("The recipe contains the following ingredients: ${ingredients.joinToString(", ")}")
-
-    println("\nIngredients list:")
-    for (ingredient in ingredients) {
-        println("- $ingredient")
-    }
+    println("The recipe contains the following ingredients:")
+    ingredients.forEach { println(it) }
 }
+
