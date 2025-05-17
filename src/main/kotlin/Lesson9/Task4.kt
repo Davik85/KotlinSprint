96a1045faadd0f1e9563d5f32660a8e61f9b2662
David <Davik50@gmail.com>
2025-05-17 03:17:34 +0200

    KS-9-4 Task4 input ingredients v2

--- src/main/kotlin/Lesson9/Task4.kt
+++ src/main/kotlin/Lesson9/Task4.kt
@@ -2,17 +2,16 @@ package org.example.Lesson9
 
 fun main() {
     print("Enter 5 ingredients separated by a comma and a space (e.g., sugar, milk, eggs, flour, butter): ")
-    val input = readln()
 
-    val ingredients = input.split(", ")
+    val ingredients = readln().split(", ")
 
     if (ingredients.size != 5) {
-        println("Please enter exactly 5 ingredients separated by \", \".")
+        println("Please enter exactly 5 ingredients.")
         return
     }
 
     val sortedIngredients = ingredients.sorted()
 
-    println("Ingredients in alphabetical order:")
-    println(sortedIngredients.joinToString(", "))
+    println("Ingredients in alphabetical order: ${sortedIngredients.joinToString(", ")}")
 }
+
