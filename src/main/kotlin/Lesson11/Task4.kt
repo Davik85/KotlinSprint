dbdd214327c9c4a73b54f702bd9714b0f4993894
David <Davik50@gmail.com>
2025-05-21 15:37:01 +0200

    KS-11-4 Task4  recipe class

---
+++ src/main/kotlin/Lesson11/Task4.kt
@@ -0,0 +1,24 @@
+package org.example.Lesson11
+
+data class RecipeCategory(
+    val id: String,
+    val title: String,
+    val imageUrl: String
+)
+
+data class Recipe(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val categoryId: String,
+    val durationMinutes: Int,
+    val servings: Int,
+    val ingredients: List<Ingredient>,
+    val instructions: List<String>
+)
+
+data class Ingredient(
+    val name: String,
+    val quantity: Float,
+    val unit: String
+)
