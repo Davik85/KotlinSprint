184396b4f006ea7e6e0965a2eaba78041f8f0d90
David <Davik50@gmail.com>
2025-05-31 04:32:16 +0200

    KS-16-1 task1 dice

---
+++ src/main/kotlin/Lesson16/Task1.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson16
+
+import kotlin.random.Random
+
+fun main() {
+    val dice = Dice()
+    dice.reveal()
+}
+
+class Dice {
+
+    private val value: Int = Random.nextInt(1, 7)
+
+    fun reveal() {
+        println("🎲 The dice shows: $value")
+    }
+}
