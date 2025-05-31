f1c0da24c2bb63bdc5035dc351f52e9f1f24898b
David <Davik50@gmail.com>
2025-05-31 06:20:16 +0200

    KS-16-3 task3 private pass

---
+++ src/main/kotlin/Lesson16/Task3.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson16
+
+fun main() {
+    val user = User("Snake", "secure123")
+
+    val input = "secure123"
+    val result = user.isPasswordCorrect(input)
+
+    println("✅ Password correct: $result")
+}
+
+class User(val login: String, private val password: String) {
+
+    fun isPasswordCorrect(input: String): Boolean {
+        return input == password
+    }
+}
